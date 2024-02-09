7f0d98f525dc4458837866e0f118e0debc83f0bd
maddiehutch1 <madhutch@byu.edu>
2024-02-08 19:24:36 -0700

    Add project files.

---
+++ MadHutch-OrigamiApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadHutch_OrigamiApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
