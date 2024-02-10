3a7f09012df61e06772e7208172646335389d775
maddiehutch1 <maddiehutch20@gmail.com>
2024-02-09 22:46:19 -0700

    added functionality to see if input is integer and tested css

--- MadHutch-OrigamiApp/wwwroot/js/site.js
+++ MadHutch-OrigamiApp/wwwroot/js/site.js
@@ -8,10 +8,17 @@ function calCost()
     let form = document.getElementById("tutorEstimate");
     let userData = new FormData(form);
 
-    let userHrs = parseFloat(userData.get("numOfHours"));
-    let hourlyWage = parseFloat(userData.get("costPerHour"));
+    if (userData.get("numOfHours") > 0) {
+        let userHrs = parseFloat(userData.get("numOfHours"));
 
-    let totalCost = Math.round((userHrs * hourlyWage), 2);
+        let hourlyWage = parseFloat(userData.get("costPerHour"));
 
-    document.getElementById("totalOutput").innerHTML = totalCost;
+        let totalCost = Math.round((userHrs * hourlyWage), 2);
+
+        document.getElementById("totalOutput").innerHTML = "$" + totalCost.toString();
+    }
+    else {
+        alert("Aw man! Our calculator cannot use that value. Please reenter a number above 0 hours.");
+    }
+    
 }
